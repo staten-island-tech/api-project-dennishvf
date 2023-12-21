f51bd5dda4c195923de57fc8291462ae48165d92
dennishvf <dennisk38@nycstudents.net>
2023-12-21 09:10:45 -0500

    lkjn

---
+++ api-project/javascript/dom.js
@@ -0,0 +1,7 @@
+const Domselectors = {
+    Form: document.getElementById("form"),
+    SearchBar: document.getElementById("Search-Bar"),
+    Card: document.querySelectorAll(".card"),
+    NameOfUni: document.querySelectorAll(".Uname")
+}
+export {Domselectors}
--- javascript/dom.js
+++
@@ -1,5 +0,0 @@
-const Domselectors = {
-    Form: document.getElementById("form"),
-    SearchBar: document.getElementById("Search-Bar")
-}
-export {Domselectors}
