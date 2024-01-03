92237902ca162ef4f440e8848aac1962535e6588
dennishvf <dennisk38@nycstudents.net>
2024-01-03 08:45:20 -0500

    lkhklhlk

--- api-project/javascript/dom.js
+++ api-project/javascript/dom.js
@@ -1,6 +1,7 @@
 const Domselectors = {
     Form: document.getElementById("form"),
     SearchBar: document.getElementById("Search-Bar"),
+    EnterButton: document.querySelector(".button"),
     FlexContainer: document.querySelector(".flex-container"),
     Card: document.querySelectorAll(".card"),
     NameOfUni: document.querySelectorAll(".Uname"),
--- api-project/javascript/main.js
+++ api-project/javascript/main.js
@@ -34,4 +34,4 @@ function Insert(arr){
      });
      
  }
- Insert(menu)
+ Insert(URL)
