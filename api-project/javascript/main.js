456d84ceee4784c5737fcaea580a478ff81cde94
dennishvf <dennisk38@nycstudents.net>
2024-01-08 08:33:24 -0500

    blkblkbk

--- api-project/javascript/main.js
+++ api-project/javascript/main.js
@@ -1,81 +1,83 @@
 import { Domselectors } from "./dom";
 import "../css/style.css";
 
-const iurl=`http://universities.hipolabs.com/search?country=United States`
-function iInsert(iarr){
-    iarr.forEach((School) => {
-         document.querySelector(".Flex-container").insertAdjacentHTML("afterbegin",
-     `<div class="card">
+const iurl = `http://universities.hipolabs.com/search?country=United States`;
+function iInsert(iarr) {
+  iarr.forEach((School) => {
+    document.querySelector(".Flex-container").insertAdjacentHTML(
+      "afterbegin",
+      `<div class="card">
      <h1 class="Uname">${School.name}</h1>
      <h2 class="Ulink">${School.web_pages}</h2>
-     </div>`)
-     });
- }
+     </div>`
+    );
+  });
+}
 
-async function igetData(URL){
-    try{
-        const response= await fetch (URL);
-        if (response.status !=200){
-            throw new Error(response.statusText);
-        }
-        console.log(response);
-        const data= await response.json();
-        console.log(data);
-        iInsert(data)
-        //if data length 0 then error
-        if(data.length === 0){
-            Domselectors.error.textContent = "Error"
-            Domselectors.FlexContainer.innerHTML=""
-        }
-    } catch (error){ Domselectors.error.textContent = "Error";}
-   
+async function igetData(URL) {
+  try {
+    const response = await fetch(URL);
+    if (response.status != 200) {
+      throw new Error(response.statusText);
+    }
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    iInsert(data);
+    //if data length 0 then error
+    if (data.length === 0) {
+      Domselectors.error.textContent = "Error";
+      Domselectors.FlexContainer.innerHTML = "";
+    }
+  } catch (error) {
+    Domselectors.error.textContent = "Error";
+  }
 }
 igetData(iurl);
 
-
-
-//search bar 
+//search bar
 Domselectors.Form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    Domselectors.FlexContainer.innerHTML=""
-    var ser = `${Domselectors.SearchBar.value}`;
-    console.log(ser)
-    const URL = `http://universities.hipolabs.com/search?country=${ser}`
+  event.preventDefault();
+  Domselectors.FlexContainer.innerHTML = "";
+  var ser = `${Domselectors.SearchBar.value}`;
+  console.log(ser);
+  const URL = `http://universities.hipolabs.com/search?country=${ser}`;
 
-function Insert(arr){
+  function Insert(arr) {
     arr.forEach((School) => {
-         document.querySelector(".Flex-container").insertAdjacentHTML("afterbegin",
-     `<div class="card">
+      document.querySelector(".Flex-container").insertAdjacentHTML(
+        "afterbegin",
+        `<div class="card">
      <h1 class="Uname">${School.name}</h1>
      <h2 class="Ulink">${School.web_pages}</h2>
-     </div>`)
-     });
- }
+     </div>`
+      );
+    });
+  }
 
- async function getData(URL){
-    try{
-        const response= await fetch (URL);
-        if (response.status !=200){
-            throw new Error(response.statusText);
-        }
-        console.log(response);
-        const data= await response.json();
-        console.log(data);
-        Insert(data)
-        //if data length 0 then error
-        if(data.length === 0){
-            Domselectors.error.textContent = "Error"
-            Domselectors.FlexContainer.innerHTML=""
-        }
-        else{Domselectors.error.textContent=""}
-    } catch (error){ Domselectors.error.textContent = "Error";}
-   
-}
-getData(URL);
-})
-
-
+  async function getData(URL) {
+    try {
+      const response = await fetch(URL);
+      if (response.status != 200) {
+        throw new Error(response.statusText);
+      }
+      console.log(response);
+      const data = await response.json();
+      console.log(data);
+      Insert(data);
+      //if data length 0 then error
+      if (data.length === 0) {
+        Domselectors.error.textContent = "Error";
+        Domselectors.FlexContainer.innerHTML = "";
+      } else {
+        Domselectors.error.textContent = "";
+      }
+    } catch (error) {
+      Domselectors.error.textContent = "Error";
+    }
+  }
+  getData(URL);
+});
 
 //const benter = document.querySelector(".button")
 //benter.addEventListener("click", (event) => {
-
